d953614a5e5d1bba1ade9d057e4059e7f4663464
cristian1clj <cristian1clj@gmail.com>
2022-08-02 01:09:47 -0500

    get-home and post-create_user

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
+# Python
+from typing import Optional
+
+# Pydantic
+from pydantic import BaseModel
+
+# FastAPI
 from fastapi import FastAPI
-from fastapi import FastAPI
+from fastapi import Body
+
 
 app = FastAPI()
+
+# Models
+class User(BaseModel):
+    user_name: str
+    mail: str
+    first_name: str
+    last_name: str
+    age: Optional[int] = None
+
 
 @app.get("/")
 def home():
     return {"Hello": "world"}
+
+
+# Request and response body
+@app.post("/user/new")
+def create_user(user: User = Body(...)):
+    return user
